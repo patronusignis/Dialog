c4d306c89095c3e097eb5bf465fb0d20c26d3a45
patronusignis <Fantik97@seznam.cz>
2014-05-06 20:10:18 +0200

    add simple toast

--- src/main/java/com/vaclav/kuba/testing/firstdialog/dialog/MainActivity.java
+++ src/main/java/com/vaclav/kuba/testing/firstdialog/dialog/MainActivity.java
@@ -2,6 +2,9 @@ package com.vaclav.kuba.testing.firstdialog.dialog;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -9,6 +12,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button showDialog = (Button) findViewById(R.id.search_button);
+        showDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "HUDRY HUDRY HUDRY", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
