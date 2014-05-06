ae8fdcfefa4ae3f09069a43cd4d89cf7c64e522e
patronusignis <Fantik97@seznam.cz>
2014-05-06 16:19:26 +0200

    cleanup code

--- src/main/java/com/vaclav/kuba/testing/firstdialog/dialog/MainActivity.java
+++ src/main/java/com/vaclav/kuba/testing/firstdialog/dialog/MainActivity.java
@@ -1,9 +1,7 @@
 package com.vaclav.kuba.testing.firstdialog.dialog;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -14,24 +12,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {g
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 }
