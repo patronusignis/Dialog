a8c42861cb948cf0d0bf979aa60b5635b6f36e9f
patronusignis <Fantik97@seznam.cz>
2014-05-07 15:49:42 +0200

    added cancel button

--- src/main/java/com/vaclav/kuba/testing/firstdialog/dialog/MainActivity.java
+++ src/main/java/com/vaclav/kuba/testing/firstdialog/dialog/MainActivity.java
@@ -1,5 +1,8 @@
 package com.vaclav.kuba.testing.firstdialog.dialog;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -13,10 +16,22 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button showDialog = (Button) findViewById(R.id.showDialog);
+        final Activity activity = this;
         showDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "HUDRY HUDRY HUDRY", Toast.LENGTH_LONG).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                builder.setMessage("HUDRY");
+                builder.setTitle("Party");
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "YOLO FULL SWAG", Toast.LENGTH_LONG).show();
+                    }
+
+                });
+                builder.show();
             }
         });
     }
